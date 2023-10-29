97cee00f8a63beb80bc2dfba128801e7f795f13b
L3PRNK <81587770+L3PRNK@users.noreply.github.com>
2023-10-29 19:31:47 +0530

    Add files via upload

--- job1.java
+++ job1.java
@@ -1,5 +1,19 @@
-public class job1 {
+import java.util.Scanner;
+
+public class project1 {
     public static void main(String[] args) {
-        System.out.println("This is job1");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        scanner.close();
     }
 }
