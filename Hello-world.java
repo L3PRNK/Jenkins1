d9fba2bff586139e6ba05a8c75c19a5adc0f8ba0
L3PRNK <81587770+L3PRNK@users.noreply.github.com>
2023-10-29 19:08:40 +0530

    Update Hello-world.java

--- Hello-world.java
+++ Hello-world.java
@@ -1,6 +1,5 @@
-public class Hello-world
-{
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
